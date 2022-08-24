ff1ea936dd04094b72de139c5fe0291d8736ab30
MandyTrl <mandythorel@gmail.com>
2022-08-24 18:16:25 +0200

    CSS + counter + remove msg

--- script.js
+++ script.js
@@ -1,2 +1,15 @@
-let NMsg = document.getElementsByClassName("msg").length;
-document.getElementsByClassName("counter").textContent = "Messages reçus : NMsg";
+///COMPTEUR///
+let NMsg = document.getElementsByClassName("msg").length ///Variable qui permet de comptabiliser le nombre de MSG
+console.log("MSG ::: "+NMsg);
+document.getElementById("count").textContent = NMsg; ///Permet de MAJ le compteur de MSG
+
+///SUPPRESSION MSG///
+for(var i=0; i<document.getElementsByClassName("trash").length; i++){
+    document.getElementsByClassName("trash")[i].addEventListener("click",
+        function(){
+            this.parentNode.remove();///supprime le msg (élément parent de la poubelle cliquée graçe au"this")
+            let NMsg = document.getElementsByClassName("msg").length ///Variable qui permet de comptabiliser le nombre de MSG
+            document.getElementById("count").textContent = NMsg; ///Permet de MAJ le compteur de MSG
+        }
+    );
+}; ///Ecoute de l'événement et modification au click
