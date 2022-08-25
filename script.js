7c9bf790aea3906212469d004fd51532b111e14b
MandyTrl <mandythorel@gmail.com>
2022-08-25 16:36:04 +0200

    hide scrollbar + logo ok + essai search bar

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 ///COMPTEUR///
-let NMsg = document.getElementsByClassName("msg").length ///Variable qui permet de comptabiliser le nombre de MSG
+let NMsg = document.getElementsByClassName("msg").length; ///Variable qui permet de comptabiliser le nombre de MSG
 console.log("MSG ::: "+NMsg);
 document.getElementById("count").textContent = NMsg; ///Permet de MAJ le compteur de MSG
 
@@ -13,3 +13,17 @@ for(var i=0; i<document.getElementsByClassName("trash").length; i++){
         }
     );
 }; ///Ecoute de l'événement et modification au click
+
+///RECHERCHER UN MESSAGE///
+document.getElementById("search").addEventListener("click",
+    function(){
+        let value = document.getElementById("saisie").value;
+        let sender = document.getElementByTagName("h2");
+        console.log("Saisie récupérée : "+value);
+        sender.each(function(){
+            if(value != this.textContent()){
+                this.parent().parent().parent().fadeOut();
+            }
+        })
+    }
+); ///Détection du click sur le bouton "search" + récupère la valeur saisie dans la barre de recherche
