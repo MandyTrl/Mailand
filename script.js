3c764b4eedec10e3311a301d7b31160ed6cd2898
MandyTrl <mandythorel@gmail.com>
2022-09-05 18:19:47 +0200

    essai .filter

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ for(var i=0; i<document.getElementsByClassName("trash").length; i++){
     document.getElementsByClassName("trash")[i].addEventListener("click",
         function(){
             this.parentNode.remove();///supprime le msg (élément parent de la poubelle cliquée graçe au"this")
-            let NMsg = document.getElementsByClassName("msg").length ///Variable qui permet de comptabiliser le nombre de MSG
+            let NMsg = document.getElementsByClassName("msg").length; ///Variable qui permet de comptabiliser le nombre de MSG
             document.getElementById("count").textContent = NMsg; ///Permet de MAJ le compteur de MSG
         }
     );
@@ -18,12 +18,14 @@ for(var i=0; i<document.getElementsByClassName("trash").length; i++){
 document.getElementById("search").addEventListener("click",
     function(){
         let value = document.getElementById("saisie").value;
-        let sender = document.getElementByTagName("h2");
+        let sender = document.getElementsByTagName("h2");
         console.log("Saisie récupérée : "+value);
-        sender.each(function(){
-            if(value != this.textContent()){
-                this.parent().parent().parent().fadeOut();
-            }
-        })
+        sender.filter(item => sender.includes(value))
+        // sender.each(function(){
+        //     if(value != this.textContent()){
+        //         .filter(item => tableau.includes(search))
+        //         this.parent().parent().parent().fadeOut();
+        //     }
+        // })
     }
 ); ///Détection du click sur le bouton "search" + récupère la valeur saisie dans la barre de recherche
