deefa748e1022b4c8d17af58564e22881de4e345
MandyTrl <mandythorel@gmail.com>
2022-09-27 13:51:07 +0200

    essai filter 2

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
 ///COMPTEUR///
 let NMsg = document.getElementsByClassName("msg").length; ///Variable qui permet de comptabiliser le nombre de MSG
-console.log("MSG ::: "+NMsg);
 document.getElementById("count").textContent = NMsg; ///Permet de MAJ le compteur de MSG
 
 ///SUPPRESSION MSG///
-for(var i=0; i<document.getElementsByClassName("trash").length; i++){
+for(let i=0; i<document.getElementsByClassName("trash").length; i++){
     document.getElementsByClassName("trash")[i].addEventListener("click",
         function(){
             this.parentNode.remove();///supprime le msg (élément parent de la poubelle cliquée graçe au"this")
@@ -17,16 +16,17 @@ for(var i=0; i<document.getElementsByClassName("trash").length; i++){
 ///RECHERCHER UN MESSAGE///
 document.getElementById("search").addEventListener("click",
     function(){
-        let value = document.getElementById("saisie").value;
+        let value = document.getElementById("saisie").value; ///Variable qui récupère la saisie de l'input
         let sender = document.getElementsByTagName("h2");
         let msg = document.getElementsByClassName("msg");
-        console.log("Saisie récupérée : ",value);
-        // msg.filter(item => sender.includes(value));
-        for(i=0; i<sender.length; i++){
-            if(sender.length[i] === value){
-                msg.show(sender.length[i])
+        console.log("type sender : ", typeof(sender), " type msg : ", typeof(msg));
+        console.log("tableau msg :", msg);
+        console.log("tableau sender :", sender);
+        for (let i=0; i < sender.length; i++){
+            let senderSelected = sender[i].includes(value)
+            for (let i=0; i < msg.length; i++){
+                msg.filter(item => senderSelected);
             }
         }
-        // msg.show(sender.includes(value));
     }
 ); ///Détection du click sur le bouton "search" + récupère la valeur saisie dans la barre de recherche
